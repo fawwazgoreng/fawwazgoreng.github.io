45a96ef8805d78d6f1617c90857cb11393aa95e1
fawwazgoreng <hatorimikael@gmail.com>
2026-05-04 10:11:32 +0700

    fix: Error content doesn't detect

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,29 +1,19 @@
-import { defineCollection } from "astro:content";
-import { z } from "astro/zod";
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const behavior = defineCollection({
-    type: "data",
-    schema: z.array(
-        z.object({
-            type: z.string(),
-            title1: z.string(),
-            title2: z.string().optional(),
-            description: z.string(),
-            reason: z.string(),
-        }),
-    ),
+    loader: glob({ pattern: "**/*.json", base: "./src/content/behavior" }),
+    schema: z.object({
+        items: z.array(
+            z.object({
+                type: z.string(),
+                title1: z.string(),
+                title2: z.string(),
+                description: z.string(),
+                reason: z.string(),
+            })
+        ),
+    }),
 });
 
-const projects = defineCollection({
-    type: "data",
-    schema: z.array(
-        z.object({
-            title: z.string(),
-            description: z.string(),
-            status: z.enum(["Stable", "In-Progress", "Legacy"]),
-            techStack: z.array(z.string()),
-        }),
-    ),
-});
-
-export const collections = { behavior, projects };
+export const collections = { behavior };
